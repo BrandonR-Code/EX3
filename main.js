81f5c6011b7358a6adb0e0fe2322258f2f7234ce
Brandon Rivera <bnr25@scarletmail.rutgers.edu>
2023-06-03 16:59:55 -0400

    Clock is Broken

--- main.js
+++ main.js
@@ -7,8 +7,27 @@ let clock = {
     setSeconds: function(s) {this.seconds = s}
 };
 
+//Dates and Times
 let now = new Date()
 clock.setHours(now.getHours());
 clock.setMinutes(now.getMinutes());
 clock.setSeconds(now.getSeconds());
 console.log(clock)
+
+//Intervals
+setInterval(function() {
+    clock.seconds++
+    if(clock.seconds === 60){
+        clock.seconds = 0;
+        clock.minutes++;
+    }
+    if(clock.minutes === 60){
+        clock.minutes = 0;
+        clock.hours++;
+    }
+    if(clock.hours === 24){
+        clock.hours = 0;
+    }
+}, 1000);
+
+console.log(clock);
